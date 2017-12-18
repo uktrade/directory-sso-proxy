1ee3b9572e304348ad6db97cd25925830f422ae0
richtier <rikatee@gmail.com>
2017-12-18 17:44:37 +0000

    Add proxy for sso ping

--- proxy/healthcheck/tests/test_views.py
+++ proxy/healthcheck/tests/test_views.py
@@ -9,3 +9,13 @@ def test_health_check_proxy_signed(signed_client):
 def test_session_user_proxy_unsigned(client):
     response = client.get(reverse('session_user_proxy'))
     assert response.status_code == 403
+
+
+def test_ping_proxy_proxy_signed(signed_client):
+    response = signed_client.get(reverse('ping_proxy'))
+    assert response.status_code == 200
+
+
+def test_ping_proxy_proxy_unsigned(client):
+    response = client.get(reverse('ping_proxy'))
+    assert response.status_code == 403
--- proxy/healthcheck/views.py
+++ proxy/healthcheck/views.py
@@ -3,3 +3,7 @@ from proxy.utils import CheckSignatureMixin, BaseProxyView
 
 class HealthCheckAPIProxyView(CheckSignatureMixin, BaseProxyView):
     pass
+
+
+class PingAPIProxyView(CheckSignatureMixin, BaseProxyView):
+    pass
