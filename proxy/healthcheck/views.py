13d4aff8fc8a99f7a0c1d11bf9d77b11f9dae223
richtier <rikatee@gmail.com>
2017-12-19 11:36:50 +0000

    Remove signature check from healthcheck

--- proxy/healthcheck/views.py
+++ proxy/healthcheck/views.py
@@ -1,7 +1,7 @@
 from proxy.utils import CheckSignatureMixin, BaseProxyView
 
 
-class HealthCheckAPIProxyView(CheckSignatureMixin, BaseProxyView):
+class HealthCheckAPIProxyView(BaseProxyView):
     pass
 
 
