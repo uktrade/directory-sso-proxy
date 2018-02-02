cc4e56eba8832caf6e951eff2bffd7e19f0a83f8
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-02 14:44:28 +0000

    add extra dispatch method to return 404 when TEST API is disabled

--- testapi/views.py
+++ testapi/views.py
@@ -1,5 +1,12 @@
+from django.http import Http404
+
+from config import settings
 from proxy.utils import BaseProxyView, CheckSignatureMixin
 
 
 class UserByEmailAPIView(CheckSignatureMixin, BaseProxyView):
-    pass
+
+    def dispatch(self, *args, **kwargs):
+        if not settings.TEST_API_ENABLE:
+            raise Http404()
+        return super().dispatch(*args, **kwargs)
