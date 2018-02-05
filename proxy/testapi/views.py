e555fb7b98293aeff437293181d5b287c90b1645
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-05 14:16:09 +0000

    remove redundant dispatch handler

--- proxy/testapi/views.py
+++ proxy/testapi/views.py
@@ -1,12 +1,5 @@
-from django.http import Http404
-
-from config import settings
 from proxy.utils import BaseProxyView
 
 
 class UserByEmailAPIView(BaseProxyView):
     pass
-    # def dispatch(self, *args, **kwargs):
-    #     if not settings.TEST_API_ENABLE:
-    #         raise Http404()
-    #     return super().dispatch(*args, **kwargs)
