a1f8a2fc0e9a0e08120e93621fead454dec4aa89
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-05 14:16:27 +0000

    update assertions

--- proxy/testapi/tests/test_views.py
+++ proxy/testapi/tests/test_views.py
@@ -24,7 +24,7 @@ def test_get_user_by_email_with_signed_client_and_invalid_token(signed_client):
     url = reverse('user_by_email', kwargs={"email": "some@user.com"})
     query = {"token": "this_is_an_invalid_token"}
     response = signed_client.get(url, query)
-    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 def test_signed_request_without_user_required_email(signed_client):
@@ -40,24 +40,24 @@ def test_get_user_by_email_with_unsigned_client_and_testing_api_disabled(
     url = reverse('user_by_email', kwargs={"email": "some@user.com"})
     query = {"token": TEST_API_AUTH_TOKEN}
     response = client.get(url, query)
-    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 def test_get_user_by_email_with_unsigned_client(client):
     url = reverse('user_by_email', kwargs={"email": "some@user.com"})
     query = {"token": TEST_API_AUTH_TOKEN}
     response = client.get(url, query)
-    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 def test_get_user_by_email_with_unsigned_client_and_invalid_auth_token(client):
     url = reverse('user_by_email', kwargs={"email": "some@user.com"})
     query = {"token": "this_is_an_invalid_token"}
     response = client.get(url, query)
-    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 def test_get_user_by_email_with_unsigned_client_without_auth_token(client):
     url = reverse('user_by_email', kwargs={"email": "some@user.com"})
     response = client.get(url, format='json')
-    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
