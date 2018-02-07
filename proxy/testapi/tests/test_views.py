522417e94b8a29d87b5d671d7160056484bb75be
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-07 11:34:01 +0000

    use signle-quote consistently

--- proxy/testapi/tests/test_views.py
+++ proxy/testapi/tests/test_views.py
@@ -5,19 +5,19 @@ from rest_framework import status
 def test_get_user_by_email_with_signed_client_and_testing_api_disabled(
         settings, signed_client):
     settings.TEST_API_ENABLE = False
-    url = reverse('user_by_email', kwargs={"email": "some@user.com"})
+    url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
     response = signed_client.get(url)
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 def test_get_user_by_email_with_signed_client(signed_client):
-    url = reverse('user_by_email', kwargs={"email": "some@user.com"})
+    url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
     response = signed_client.get(url)
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 def test_signed_request_without_user_required_email(signed_client):
-    url = reverse('user_by_email', kwargs={"email": ""})
+    url = reverse('user_by_email', kwargs={'email': ''})
     response = signed_client.get(url)
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
@@ -25,12 +25,12 @@ def test_signed_request_without_user_required_email(signed_client):
 def test_get_user_by_email_with_unsigned_client_and_testing_api_disabled(
         settings, client):
     settings.TEST_API_ENABLE = False
-    url = reverse('user_by_email', kwargs={"email": "some@user.com"})
+    url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
     response = client.get(url)
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 def test_get_user_by_email_with_unsigned_client(client):
-    url = reverse('user_by_email', kwargs={"email": "some@user.com"})
+    url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
     response = client.get(url)
     assert response.status_code == status.HTTP_403_FORBIDDEN
