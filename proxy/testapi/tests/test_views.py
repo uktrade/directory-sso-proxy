8c5ed5acf38f94d8c17ac1de2aef2ac31fc1e94a
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-14 06:36:07 +0000

    add tests for flagging user emails as verified or not

--- proxy/testapi/tests/test_views.py
+++ proxy/testapi/tests/test_views.py
@@ -68,3 +68,43 @@ def test_delete_user_by_email_with_unsigned_client(client):
     url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
     response = client.delete(url)
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_flag_user_email_with_signed_client_and_testing_api_disabled(
+        settings, signed_client):
+    settings.FEATURE_TEST_API_ENABLE = False
+    url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
+    data = {'is_verified': True}
+    response = signed_client.patch(url, data=data)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_flag_user_email_with_signed_client(signed_client):
+    url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
+    data = {'is_verified': False}
+    response = signed_client.patch(url, data=data)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_flag_user_email_with_signed_client_without_required_email(
+        signed_client):
+    url = reverse('user_by_email', kwargs={'email': ''})
+    data = {'is_verified': True}
+    response = signed_client.patch(url, data=data)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_flag_user_email_with_unsigned_client_and_testing_api_disabled(
+        settings, client):
+    settings.FEATURE_TEST_API_ENABLE = False
+    url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
+    data = {'is_verified': False}
+    response = client.patch(url, data=data)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_flag_user_email_with_unsigned_client(client):
+    url = reverse('user_by_email', kwargs={'email': 'some@user.com'})
+    data = {'is_verified': True}
+    response = client.patch(url, data=data)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
